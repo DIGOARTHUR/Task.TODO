be82802430ac4704b3a277caa0a3f5b5bbb6750c
Diego Arthur <digo.arthur@hotmail.com>
2022-02-10 14:45:47 -0300

    new format aplication

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import '../styles/header.scss'
+
+export function Header() {
+  return (
+    <header className="header">
+      <div>
+        <img src="/logo.svg" alt="to.do" />
+      </div>
+
+    </header>
+  )
+}
---
+++ src/components/TaskList.tsx
@@ -0,0 +1,120 @@
+import { useState } from 'react'
+
+import '../styles/tasklist.scss'
+
+import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+import { tsAnyKeyword } from '@babel/types';
+
+interface Task {
+  id: number;
+  title: string;
+  isComplete: boolean;
+}
+
+export function TaskList() {
+  const [message, setMessage] = useState('');
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTaskTitle, setNewTaskTitle] = useState('');
+
+
+  function handleCreateNewTask() {
+    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return (setMessage('Digite alguma Tarefa !'),
+
+      setTimeout(function () {
+        setMessage('')
+      }, 2500)
+    )
+
+
+    // Valida se a tarefa digitada no Input existe na lista. 
+    let repeatTitle = tasks.filter((task) => task.title == newTaskTitle)
+    if (repeatTitle.length) return (setMessage('Esta tarefa já existe na lista!'),
+      setTimeout(function () {
+        setMessage('')
+      }, 2500)
+    )
+
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldTask => [...oldTask, newTask]);
+    setNewTaskTitle('');
+  }
+
+  function handleToggleTaskCompletion(id: number) {
+    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const selectTask = tasks.map((task) => task.id == id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } :
+      task
+    );
+
+    setTasks(selectTask)
+  }
+
+  function handleRemoveTask(id: number) {
+    // Remova uma task da listagem pelo ID
+    const filtedDeleteTask = tasks.filter((task) => task.id != id)
+    setTasks(filtedDeleteTask)
+
+  }
+
+
+  return (
+    <section className="task-list container">
+      <header>
+        <h2>Minhas tasks</h2>
+
+        <div>
+          <div className="input-group">
+            <input
+              type="text"
+              placeholder="Adicionar novo todo"
+              value={newTaskTitle}
+              onChange={(e) => setNewTaskTitle(e.target.value)}
+
+            />
+            <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
+              <FiCheckSquare size={16} color="#fff" />
+            </button>
+            
+          </div>
+          <p className="message-text">{message}</p>
+        </div>
+
+      </header>
+
+      <main>
+        <ul>
+          {tasks.map(task => (
+            <li key={task.id}>
+              <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
+                <label className="checkbox-container">
+                  <input
+                    type="checkbox"
+                    readOnly
+                    checked={task.isComplete}
+                    onClick={() => handleToggleTaskCompletion(task.id)}
+                  />
+                  <span className="checkmark"></span>
+                </label>
+                <p>{task.title}</p>
+              </div>
+
+              <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
+                <FiTrash size={16} />
+              </button>
+            </li>
+          ))}
+
+        </ul>
+      </main>
+    </section>
+  )
+}
