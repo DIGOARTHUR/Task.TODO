c26c1e3ec39e4f3fa5b79ded356d0701c8b69f35
Diego Arthur <digo.arthur@hotmail.com>
2022-02-10 21:43:28 -0300

    fix import img

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
-import '../styles/header.scss'
+import '../styles/header.scss';
+import logo from '../assets/img/logo.svg';
 
 export function Header() {
+ 
   return (
     <header className="header">
       <div>
-        <img src="/logo.svg" alt="to.do" />
+        <img src={logo}  alt="to.do" />
       </div>
 
     </header>
